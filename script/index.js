68e5f88cff738cfa936573ed10fcc6dddfdae808
sanjanaoney <iamoney012@gmail.com>
2026-09-16 13:11:46 +0600

    vocab-card-implemented

--- script/index.js
+++ script/index.js
@@ -4,6 +4,49 @@ const loadLessons=()=>{
     .then(json=>displayLesson(json.data));
 };
 
+const loadLevelWord=(id)=>{
+    const url= `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>displayLevelWord(data.data))
+}
+
+const displayLevelWord=(words)=>{
+    const wordContainer=document.getElementById("word-container")
+    wordContainer.innerHTML="";
+
+//     {
+//     "id": 5,
+//     "level": 1,
+//     "word": "Eager",
+//     "meaning": "আগ্রহী",
+//     "pronunciation": "ইগার"
+// }
+
+    words.forEach((word)=>{
+      console.log(word);
+      const card=document.createElement("div");
+      card.innerHTML=`
+       <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+    <h2 class="font-bold text-2xl">${word.word}</h2>
+    <p class="font-semibold">Meaning/Pronounciation</p>
+
+    <div class="text-2xl font-medium font-bangla">
+      "${word.meaning}/${word.pronunciation}"
+    </div>
+    <div class="flex justify-between items-center">
+      <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+      <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume"></i></button>
+    </div>
+
+  </div>
+      
+      `;
+
+      wordContainer.append(card);
+    })
+}
+
 const displayLesson=(lessons)=>{
 // console.log(lessons)
 
@@ -16,7 +59,7 @@ for(let lesson of lessons){
 //3.create element
 const btnDiv=document.createElement("div");
 btnDiv.innerHTML=`
-<button class="btn btn-outline btn-primary"
+<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"
                 ><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no} </button
               >
 
