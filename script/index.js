0906356e4509bf996883757e30c535c4df490bd5
sanjanaoney <iamoney012@gmail.com>
2026-09-17 19:46:51 +0600

    highlighted active lessons

--- script/index.js
+++ script/index.js
@@ -4,11 +4,21 @@ const loadLessons=()=>{
     .then(json=>displayLesson(json.data));
 };
 
+const removeActive=()=>{
+  const lessonButtons=document.querySelectorAll(".lesson-btn");
+  lessonButtons.forEach(btn=>btn.classList.remove("active"))
+}
+
 const loadLevelWord=(id)=>{
     const url= `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
     .then((res)=>res.json())
-    .then((data)=>displayLevelWord(data.data))
+    .then((data)=>{
+      removeActive(); //remove all active class
+      const clickBtn= document.getElementById(`lesson-btn-${id}`)
+      clickBtn.classList.add("active");// add active class to the clicked ones
+      displayLevelWord(data.data)
+    })
 }
 
 const displayLevelWord=(words)=>{
@@ -46,7 +56,7 @@ const displayLevelWord=(words)=>{
       "${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি" }/${word.pronunciation ? word.pronunciation: "Pronounciation পাওয়া  যায়নি"}"
     </div>
     <div class="flex justify-between items-center">
-      <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+      <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
       <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume"></i></button>
     </div>
 
@@ -70,7 +80,7 @@ for(let lesson of lessons){
 //3.create element
 const btnDiv=document.createElement("div");
 btnDiv.innerHTML=`
-<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"
+<button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"
                 ><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no} </button
               >
 
