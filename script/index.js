49fbfbf0cceb8969125aaf6a7e9cd89518b2582a
sanjanaoney <iamoney012@gmail.com>
2026-09-17 20:40:58 +0600

    added modal details

--- script/index.js
+++ script/index.js
@@ -21,6 +21,66 @@ const loadLevelWord=(id)=>{
     })
 }
 
+// {
+// "status": true,
+// "message": "successfully fetched a word details",
+// "data": {
+// "word": "Eager",
+// "meaning": "আগ্রহী",
+// "pronunciation": "ইগার",
+// "level": 1,
+// "sentence": "The kids were eager to open their gifts.",
+// "points": 1,
+// "partsOfSpeech": "adjective",
+// "synonyms": [
+// "enthusiastic",
+// "excited",
+// "keen"
+// ],
+// "id": 5
+// }
+// }
+
+const loadWordDetail=async(id)=>{
+  const url=`https://openapi.programming-hero.com/api/word/${id}`
+  const res= await fetch(url);
+  const details=await res.json();
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails=(word)=>{
+console.log(word);
+const detailsBox=document.getElementById("details-container");
+detailsBox.innerHTML=`<div class="">
+        <h2 class="text-2xl font-bold">${word.word}(<i class="fa-solid fa-microphone-lines"></i> : ${word.pronunciation})</h2>
+
+      </div>
+      <div class="">
+        <h2 class="font-bold">
+          Meaning
+        </h2>
+        <p>${word.meaning}</p>
+
+      </div>
+      <div class="">
+        <h2 class="font-bold">
+          Example
+        </h2>
+        <p>${word.sentence}</p>
+
+      </div>
+      <div class="">
+        <h2 class="font-bold">
+          Synonym
+        </h2>
+        <span class="btn">Syn1</span>
+        <span class="btn">Syn1</span>
+        <span class="btn">Syn1</span>
+
+      </div>`
+document.getElementById("word_modal").showModal();
+}
+
 const displayLevelWord=(words)=>{
     const wordContainer=document.getElementById("word-container")
     wordContainer.innerHTML="";
@@ -56,7 +116,7 @@ const displayLevelWord=(words)=>{
       "${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি" }/${word.pronunciation ? word.pronunciation: "Pronounciation পাওয়া  যায়নি"}"
     </div>
     <div class="flex justify-between items-center">
-      <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+      <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
       <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume"></i></button>
     </div>
 
@@ -93,4 +153,5 @@ levelContainer.append(btnDiv)
 
 
 }
-loadLessons()
+loadLessons();
+
