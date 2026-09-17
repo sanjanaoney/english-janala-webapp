a68625af16c93aa75a6b7cb844b6ac8c0dc40117
sanjanaoney <iamoney012@gmail.com>
2026-09-17 23:07:54 +0600

    sound functionality implemented

--- script/index.js
+++ script/index.js
@@ -2,6 +2,13 @@ const createElements=(arr)=>{
     const htmlElements=arr.map(el =>`<span class="btn">${el}</span>`)
     return htmlElements.join("");
 }
+//reusuable function
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 //loading condition
 const manageSpinner=(status)=>{
   if(status==true){
@@ -134,7 +141,7 @@ const displayLevelWord=(words)=>{
     </div>
     <div class="flex justify-between items-center">
       <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-      <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume"></i></button>
+      <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume"></i></button>
     </div>
 
   </div>
